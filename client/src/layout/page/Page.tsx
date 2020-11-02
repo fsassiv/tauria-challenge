878b21c89edb481c0cc2fbc980a63415b63e5eb6
Flávio Andrade <flavio.ssiv@gmail.com>
2020-11-01 21:09:23 -0300

    update

--- client/src/layout/page/Page.tsx
+++ client/src/layout/page/Page.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/header/Header';
 import ReturnButton from './../../components/ReturnButton/ReturnButton';
 import { PageProps } from './Pages.types';
 
 const Page = (props: PageProps) => {
   const { children, withHeader } = props;
+  const { location } = useHistory();
+
   return (
     <>
       {withHeader && <Header />}
       <div className="container page">
         {children}
-        <ReturnButton />
+        {location.pathname !== '/' && <ReturnButton />}
       </div>
     </>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,8 @@
-import React, { useEffect } from 'react';
+import Page from '@layout/page/Page';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import Page from '../layout/page/Page';
-import axios from '../services/api';
 
 const Home = () => {
-  useEffect(() => {
-    (async () => {
-      try {
-        const { data } = await axios.get('/toppings');
-
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
-  }, []);
   return (
     <Page>
       <h1>Welcome to Flávio's Pizzaria</h1>
